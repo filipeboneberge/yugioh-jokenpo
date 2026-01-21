28cd7b84b341b1ff0a07b28fe569de9b2a6cd1ed
filipeboneberge <filipeboneberge@hotmail.com>
2026-01-20 22:39:22 -0300

    criando-states-engine

---
+++ src/scripts/engine.js
@@ -0,0 +1,23 @@
+const state = {
+  score: {
+    playerScore: 0,
+    computerScore: 0,
+    scoreBox: document.getElementById("score_points"),
+  },
+  cardSprites: {
+    avatar: document.getElementById("card-image"),
+    name: document.getElementById("card-name"),
+    type: document.getElementById("card-type"),
+  },
+  fieldCards: {
+    player: document.getElementById("player-cards"),
+    computer: document.getElementById("computer-cards"),
+  },
+  actions: {
+    button: document.getElementById("next-duel"),
+  },
+};
+
+function init() {}
+
+init();
