d6fc2fdf91883b1ca7cc1fcb27315e572183d796
filipeboneberge <filipeboneberge@hotmail.com>
2026-02-12 23:20:34 -0300

    musica-fundo

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -190,6 +190,10 @@ function init() {
 
   drawCards(5, playerSides.player1);
   drawCards(5, playerSides.computer);
+
+  const bgm = document.getElementById("bgm");
+  bgm.play();
+  bgm.volume = 0.5;
 }
 
 init();
